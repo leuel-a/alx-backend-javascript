555113947ecba0764ac2dacce6d199d8a0b07948
nefelibatala1993 <leuela1993@gmail.com>
2023-07-26 23:08:57 +0300

    added the fourth task

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,8 @@ export default function handleProfileSignup() {
   const upload = uploadPhoto();
   const create = createUser();
 
-  Promise.all([
-    upload,
-    create,
-  ]).then((values) => {
-    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  }).catch(() => console.log('Signup system offline'));
+  return Promise.all([upload, create])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => console.log('Signup system offline'));
 }

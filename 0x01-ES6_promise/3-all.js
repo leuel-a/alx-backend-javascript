d830d46c3f14c3c41dafcbe65732c54becf650f4
nefelibatala1993 <leuela1993@gmail.com>
2023-07-26 22:17:03 +0300

    add the fourth task

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const upload = uploadPhoto();
+  const create = createUser();
+
+  Promise.all([
+    upload,
+    create,
+  ]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  });
+}

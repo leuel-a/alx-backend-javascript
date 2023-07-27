f3625dc30b33c50ac51543056ca6950aec306d86
nefelibatala1993 <leuela1993@gmail.com>
2023-07-27 09:40:43 +0300

    add the 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpUserPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
-    .then((data) => {
-      data.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : String(result.result),
-      }));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const result = [];
+      values.forEach((value) => {
+        if (value.status === 'rejected') {
+          result.push({
+            status: value.status,
+            value: value.reason.message,
+          });
+        } else {
+          result.push({
+            status: value.status,
+            value: value.value,
+          });
+        }
+      });
+      return result;
     });
 }

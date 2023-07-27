e4690ca1f53e06907b102e6a9a3bd99a8869c9a5
nefelibatala1993 <leuela1993@gmail.com>
2023-07-27 08:08:57 +0300

    Revert "added files"
    This reverts commit 1553b9832e06fb4fa5e00294b2773fd4f42e5c76.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
+    .then((data) => {
+      data.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.result),
+      }));
+    });
 }

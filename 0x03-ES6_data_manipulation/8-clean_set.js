ef9891ecb9ca7e5dd7bb874485923746a69e74fc
nefelibatala1993 <leuela1993@gmail.com>
2023-08-01 19:50:36 +0300

    update the ninth task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,6 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  const filteredArray = [...set].filter((string) => string.startsWith(startString));
-  return filteredArray.map((value) => value.slice(startString.length)).join('-');
+  const filteredArray = [...set].filter((string) => typeof string === 'string' && string.startsWith(startString));
+  return filteredArray.map((string) => string.slice(startString.length)).join('-');
 }

9ffda16cca320c93348116df5438921addcfbd72
nefelibatala1993 <leuela1993@gmail.com>
2023-09-18 18:53:13 +0300

    first task

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message to the STDOUT
+ * 
+ * @param {string} message
+ * @return {void}
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;

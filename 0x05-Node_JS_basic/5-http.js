087fc8cb482ebc320cc3161ac400600f6041c44c
nefelibatala1993 <leuela1993@gmail.com>
2023-09-19 23:32:11 +0300

    finish the sixth task

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    const originalConsoleLog = console.log;
+
+    const captures = ['This is the list of our students'];
+    console.log = (message) => {
+      captures.push(message);
+    };
+
+    try {
+      await countStudents('database.csv');
+    } catch (error) {
+      captures.length = 0;
+      captures.push(error);
+    }
+    console.log = originalConsoleLog;
+    res.end(captures.join('\n'));
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;

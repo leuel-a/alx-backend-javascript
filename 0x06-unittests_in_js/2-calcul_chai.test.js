54ceca456595882dcde72db7a3e9dfcb408d0b69
nefelibatala1993 <leuela1993@gmail.com>
2023-09-22 11:35:07 +0300

    used the chai assertion library to make assertions using expect

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,71 @@
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+const expect = require('chai').expect;
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should add the two numbers', () => {
+      expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+    });
+
+    it('should round a before adding it', () => {
+      expect(calculateNumber('SUM', 1.2, 2)).to.equal(3);
+    });
+
+    it('shound round b before adding it', () => {
+      expect(calculateNumber('SUM', 1, 1.2)).to.equal(2);
+    });
+
+    it('should return a type of number', () => {
+      expect(calculateNumber('SUM', 1.2, 1.3)).to.be.a('number');
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should subtract the two numbers', () => {
+      expect(calculateNumber('SUBTRACT', 2, 1)).to.equal(1);
+    });
+
+    it('should round a before subtracting b from it', () => {
+      expect(calculateNumber('SUBTRACT', 2.1, 1)).to.equal(1);
+    });
+
+    it('should round b before subtracting it from a', () => {
+      expect(calculateNumber('SUBTRACT', 2, 1.1)).to.equal(1);
+    });
+
+    it('should return a type of number', () => {
+      expect(calculateNumber('SUBTRACT', 2.2, 1.1)).to.be.a('number');
+    });
+
+    it('should return a negative if result is a negative number', () => {
+      expect(calculateNumber('SUBTRACT', 2, 3)).to.equal(-1);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should divide the two numbers', () => {
+      expect(calculateNumber('DIVIDE', 2, 1)).to.equal(2);
+    });
+
+    it('should return error when b is zero', () => {
+      expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
+    });
+
+    it('should return error when b is rounded to zero', () => {
+      expect(calculateNumber('DIVIDE', 2, 0.3)).to.equal('Error');
+    });
+
+    it('should round a before dividing it', () => {
+      expect(calculateNumber('DIVIDE', 2.3, 1)).to.equal(2);
+    });
+
+    it('should round b before using it to divide', () => {
+      expect(calculateNumber('DIVIDE', 2, 1.1)).to.equal(2);
+    });
+
+    it('should not round the return value if float', () => {
+      expect(calculateNumber('DIVIDE', 3, 2)).to.equal(1.5);
+    });
+  });
+});

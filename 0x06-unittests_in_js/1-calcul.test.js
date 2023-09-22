9283979bb3dc534723450a12e6f71e17bcadbf25
nefelibatala1993 <leuela1993@gmail.com>
2023-09-22 10:45:01 +0300

    add the tests for type of subtract

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -19,4 +19,22 @@ describe('calculateNumber', () => {
       assert.strictEqual(typeof calculateNumber('SUM', 1.2, 1.3), 'number');
     });
   });
+
+  describe('SUBTRACT', () => {
+    it('should subtract the two numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2, 1), 1);
+    });
+
+    it('should round a before subtracting b from it', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2.1, 1), 1);
+    });
+
+    it('should round b before subtracting it from a', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2, 1.1), 1);
+    });
+
+    it('should return a type of number', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2.2, 1.1), 1);
+    })
+  });
 });

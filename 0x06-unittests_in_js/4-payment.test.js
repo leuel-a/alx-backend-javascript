5eb64211b5ae2ef09b7b09f4e2b535b8ae645218
nefelibatala1993 <leuela1993@gmail.com>
2023-09-23 13:00:40 +0300

    finish the fourth task

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect, util } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let utilsStub = undefined;
+  let consoleSpy = undefined;
+
+  before(() => {
+    consoleSpy = sinon.spy(console, 'log');
+    utilsStub = sinon.stub(Utils, 'calculateNumber');
+    utilsStub.returns(10);
+  });
+
+  after(() => {
+    consoleSpy.restore();
+    utilsStub.restore();
+  });
+
+  it('should call the Utils.calculateNumber function', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(utilsStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(utilsStub.callCount).to.equal(1);
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+  });
+});

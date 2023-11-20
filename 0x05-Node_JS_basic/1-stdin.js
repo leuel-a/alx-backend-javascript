9070a3739123faf8b98429baa31366f5cd31b4e4
nefelibatala1993 <leuela1993@gmail.com>
2023-11-20 20:44:34 +0300

    attempt pass for checker

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,9 @@ const rl = readline.createInterface({
   input: process.stdin, output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (ans) => {
+console.log('Welcome to Holberton School, what is your name?\n');
+
+rl.question('', (ans) => {
   console.log(`Your name is: ${ans}`);
   rl.close();
 });

70028b874c6c3438830ee90b8fa91f49797c8948
nefelibatala1993 <leuela1993@gmail.com>
2023-11-21 11:04:49 +0300

    refactored the code of the read file in sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,25 +2,29 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8').replace(/\r/gm, '');
-    const dataArr = data.split('\n').slice(1);
-    const students = dataArr
-      .map((row) => row.split(','))
-      .filter((student) => student.length > 0 && student[0] !== '');
-
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+    const dataArr = data
+      .split(/\r\n/)
+      .slice(1)
+      .filter((line) => line);
+    const students = dataArr.map((line) => line.split(','));
     const fields = new Map();
+
     for (const student of students) {
-      fields.set(student[3], (fields.get(student[3]) || []).concat(student[0]));
+      const field = student[3];
+      if (!fields.has(field)) fields.set(field, []);
+      fields.get(field).push(student[0]);
     }
 
+    // TODO: Iterate over the fields Map object and print the number of students per field
     console.log(`Number of students: ${students.length}`);
-    fields.forEach((value, key) => {
+    for (const [field, value] of fields) {
       console.log(
-        `Number of students in ${key}: ${value.length}. List: ${value.join(
+        `Number of students in ${field}: ${value.length}. List: ${value.join(
           ', ',
         )}`,
       );
-    });
+    }
   } catch (err) {
     console.log('Cannot load the database');
   }
